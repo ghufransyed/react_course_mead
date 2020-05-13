dca3d0b492a72dba7e1f251078ef6fb89b961fba
Ghufran Syed <ghufran.syed@gmail.com>
2020-05-13 10:26:06 -0700

    Complete Exploring JSX challenge01

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,22 +3,70 @@
 console.log('App.js is running!');
 
 var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
+    'div',
     null,
-    'Indecision App'
-  ),
-  '  ',
-  React.createElement(
-    'p',
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+var template2 = React.createElement(
+    'div',
     null,
-    'More stuff'
-  )
+    React.createElement(
+        'h1',
+        null,
+        'This is my new header!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is my new body text!'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'This is my first item'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Second Item!!!'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Third Item!!!'
+        )
+    )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+//ReactDOM.render(template, appRoot)
+ReactDOM.render(template2, appRoot);
 //# sourceMappingURL=app.js.map
--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 console.log('App.js is running!');
 
-var template = <div><h1>Indecision App</h1>  <p>More stuff</p></div>;
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var template2 = (
+    <div>
+        <h1>This is my new header!</h1>
+        <p>This is my new body text!</p>
+        <ol>
+            <li>This is my first item</li>
+            <li>Second Item!!!</li>
+            <li>Third Item!!!</li>
+        </ol>
+    </div>
+);
 
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot)
+//ReactDOM.render(template, appRoot)
+ReactDOM.render(template2, appRoot)
