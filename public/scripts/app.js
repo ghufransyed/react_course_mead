281986490bf4c06d55713c13fe030032c59a4b2d
Ghufran Syed <ghufran.syed@gmail.com>
2020-06-03 16:12:57 -0700

    start using es6 import/export

---
+++ public/scripts/app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+console.log('app.js is running');
+//# sourceMappingURL=app.js.map
---
+++ public/scripts/playground/es6-arrow-function.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// es5 functions
+
+var square = function square(x) {
+    return x * x;
+};
+
+console.log(square(8));
+
+// es6 arrow functions
+var squareArrow = function squareArrow(x) {
+    return x * x;
+};
+
+console.log(squareArrow(8));
+
+var getFirstname = function getFirstname(fullName) {
+    return fullName.split(' ')[0];
+};
+
+console.log(getFirstname('foobar smith'));
+//# sourceMappingURL=es6-arrow-function.js.map
