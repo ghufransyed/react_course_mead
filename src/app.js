b17d28dbcfa66f5733b9582902e1eeea428b58d6
Ghufran Syed <ghufran.syed@gmail.com>
2020-06-05 16:19:55 -0700

    start adding 3rd party component

--- src/app.js
+++ src/app.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from "./components/IndecisionApp";
 
 
-ReactDOM.render(<IndecisionApp/>,
+ReactDOM.render(
+    <IndecisionApp/>,
     document.getElementById('app'));
